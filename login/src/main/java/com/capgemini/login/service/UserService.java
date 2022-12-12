b411086b39e7a76aefd3d26f2761dd09a2dd90b9
S <akshy.s@capgemini.com>
2022-12-12 12:25:17 +0530

    Updated service and controller

--- login/src/main/java/com/capgemini/login/service/UserService.java
+++ login/src/main/java/com/capgemini/login/service/UserService.java
@@ -1,9 +1,14 @@
 package com.capgemini.login.service;
 
-import com.capgemini.login.pojo.UserPojo;
+import com.capgemini.login.pojo.UserRequest;
+import com.capgemini.login.pojo.UserResponse;
 
 public interface UserService {
 
-    String registerUser(UserPojo user);
+    String registerUser(UserRequest user);
     String loginUser(String value,String password);
+
+    UserResponse getUserByUserName(String username);
+
+    UserResponse getUserById(int id);
 }
